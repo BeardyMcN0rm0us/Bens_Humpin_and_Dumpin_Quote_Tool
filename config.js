313bac803e327373f75faa80abe0bf29fa5f00ab
BeardyMcN0rm0us <beardymcnormous@icloud.com>
2025-08-10 14:04:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.BHD_CONFIG = {
   rates: {
     baseFeeDefault: 25,
     baseFeeMove: 50,
-    mileagePerMile: 0.25,
+    mileagePerMile: 0.30,
     twoMan: 20,
     stairsPerFloor: 5,
     disposal: {
